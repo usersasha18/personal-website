14a2a62ed633dfbab54a91797e786f6ffb4b04db
usersasha18 <x6rrl@yandex.ru>
2024-03-11 01:56:07 +1000

    Добавлена разметка

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,6 +1,13 @@
+import styles from './MainContent.module.css'
+
 function MainContent() {
   return (
-    <div>MainContent</div>
+    <div className={styles.main_wrapper}>
+      <h1 className={styles.main_wrapper_title}>
+        Сайт-Визитка фронтенд разработчика
+      </h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae nesciunt dolorum fugit adipisci animi esse id illo, deleniti obcaecati deserunt unde praesentium quaerat, sit delectus fuga excepturi laboriosam incidunt dolore!</p>
+    </div>
   )
 }
 
