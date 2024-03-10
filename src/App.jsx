f7b37f38f678a687bb73e2e4305c4f9536643ce8
usersasha18 <x6rrl@yandex.ru>
2024-03-11 01:29:49 +1000

    Добавил контейнер на всей страницы

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import Header from "./components/Header/Header";
 import MainContent from './components/MainContent/MainContent';
 import Footer from './components/Footer/Footer'
+import styled from 'styled-components';
+import './global.css'
+
+const Container = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 0 1rem;
+`
 
 function App() {
   return (
-    <>  
+    <Container>
       <Header />
       <MainContent />
       <Footer />
-    </>
+    </Container>
   )
 }
 
