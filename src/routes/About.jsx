40e35bc2c19179399fdd4af00b3580d0fc45f5cc
usersasha18 <x6rrl@yandex.ru>
2024-03-11 02:05:12 +1000

    Добавил костыли, главное пока работает

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 0 1rem;
+`
 
 export default function About() {
   return (
     <>
-        <Header />
-        <div>About</div>
-        {/* <Footer /> */}
+    <Container>
+      <Header />
+      <div>About</div>
+      {/* <Footer /> */}
+    </Container>
     </>
   )
 }
--- src/routes/Contacts.jsx
+++ src/routes/Contacts.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 0 1rem;
+`
 
 export default function Contacts() {
   return (
     <>
-        <Header />
-        <div>asdasdasd</div>
-        {/* <Footer /> */}
+    <Container>
+      <Header />
+      <div>Contacts</div>
+      {/* <Footer /> */}
+    </Container>
     </>
   )
 }
